114b2822d19e516faa452d19934662fb0e1ab15f
Praval Pratap Singh <113175059+Pravalpratap@users.noreply.github.com>
2024-06-04 06:08:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,35 +1,24 @@
 function fibonacci(num) {
-    if (num <= 0) {
-        throw new Error("Input should be a positive integer.");
-    } else if (num === 1) {
-        return 0;
-    } else if (num === 2) {
-        return 1;
-    }
+  let a = 0, b = 1;
+  
+  if (num === 1) {
+    return a;
+  }
 
-    let a = 0, b = 1;
-    for (let i = 2; i < num; i++) {
-        let temp = a + b;
-        a = b;
-        b = temp;
-    }
-    return b;
+  for (let i = 2; i <= num; i++) {
+    let temp = a + b;
+    a = b;
+    b = temp;
+  }
+
+  return a;
 }
 
-function calculateFibonacci() {
-    const input = document.getElementById('numberInput').value;
-    const resultElement = document.getElementById('result');
-
-    try {
-        const num = parseInt(input);
-        if (isNaN(num) || num <= 0) {
-            resultElement.textContent = "Please enter a valid positive integer.";
-        } else {
-            const result = fibonacci(num);
-            resultElement.textContent = `The ${num}th Fibonacci number is ${result}.`;
-        }
-    } catch (error) {
-        resultElement.textContent = error.message;
+function showFibonacci() {
+  let num = document.getElementById('num').value;
+  let result = fibonacci(num);
+  document.getElementById('result').textContent = "The " + num + "th Fibonacci number is: " + result;
+}
     }
 }
 
