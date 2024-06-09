f53ee8005a00241d3c266c900d71879675ea42db
Praval Pratap Singh <113175059+Pravalpratap@users.noreply.github.com>
2024-06-09 13:13:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -3,22 +3,27 @@ function fibonacci(num) {
   
   if (num === 1) {
     return a;
+  } else if (num === 2) {
+    return b;
   }
 
-  for (let i = 2; i <= num; i++) {
+  for (let i = 3; i <= num; i++) {
     let temp = a + b;
     a = b;
     b = temp;
   }
 
-  return a;
+  return b;
 }
 
 function showFibonacci() {
-  let num = document.getElementById('num').value;
+  let num = parseInt(document.getElementById('num').value, 10);
+  
+  if (isNaN(num) || num < 1 || num > 50) {
+    document.getElementById('result').textContent = "Please enter a valid number between 1 and 50.";
+    return;
+  }
+  
   let result = fibonacci(num);
   document.getElementById('result').textContent = "The " + num + "th Fibonacci number is: " + result;
 }
-    }
-}
-
